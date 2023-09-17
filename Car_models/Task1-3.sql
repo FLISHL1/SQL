b274e3d3a774a542debbd03bb09fdb30de124de3
FLISH <kirill15022005@gmail.com>
2023-09-17 17:47:52 +0300

    /

---
+++ Car_models/Task1-3.sql
@@ -0,0 +1,3 @@
+SELECT lastName FROM employees;
+SELECT lastName, firstName, jobTitle FROM employees;
+SELECT employeeNumber, lastName, firstName, extension, email, officeCode, reportsTo, jobTitle FROM employees;
---
+++ Car_models/Task4.sql
@@ -0,0 +1,6 @@
+SELECT contactLastName, contactFirstName FROM customers
+ORDER BY contactLastName;
+SELECT contactLastName, contactFirstName FROM customers
+ORDER BY contactLastName desc;
+SELECT contactLastName, contactFirstName FROM customers
+ORDER BY contactLastName, contactFirstName DESC;
