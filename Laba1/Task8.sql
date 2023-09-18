7d8c57e98bed3368a0b4d46de97549dd79d9f71f
FLISH <kirill15022005@gmail.com>
2023-09-18 13:46:10 +0300

    update Laba1

--- Laba1/Task8.sql
+++ Laba1/Task8.sql
@@ -1,11 +1,7 @@
-# SELECT CONCAT_WS(" ", `Математика`, `Русский`, GREATEST(`Физика`, `Информатика`, `Английский`)) as svod, COUNT(*) FROM
-#     (SELECT name,
-#             MAX(CASE WHEN subject='Математика' THEN mark ELSE '-' END) as `Математика`,
-#             MAX(CASE WHEN subject='Русский' THEN mark ELSE '-' END) as `Русский`,
-#             MAX(CASE WHEN subject='Физика' THEN mark ELSE '-' END) as `Физика`,
-#             MAX(CASE WHEN subject='Информатика' THEN mark ELSE '-' END) as `Информатика`,
-#             MAX(CASE WHEN subject='Английский' THEN mark ELSE '-' END) as `Английский`
-#      FROM Marks
-#      GROUP BY name
-#     ) as f
-# GROUP BY CONCAT_WS(" ", `Математика`, `Русский`, GREATEST(`Физика`, `Информатика`, `Английский`));
+SELECT comb, COUNT(comb) FROM
+(SELECT SUBSTRING_INDEX(GROUP_CONCAT(mark ORDER BY mark DESC SEPARATOR ', '), ', ', 3) as comb
+    FROM Marks
+    GROUP BY name) as f
+GROUP BY comb;
+
+
