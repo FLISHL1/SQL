12dd2e71be55ed09f68e8bc9d7e6a3272f80182b
FLISH <kirill15022005@gmail.com>
2023-09-24 21:33:41 +0300

    Update Task26.sql

--- Car_models/Задание 2/Task26.sql
+++ Car_models/Задание 2/Task26.sql
@@ -1,4 +1,5 @@
-/*SELECT f1.costs, COUNT(orderNumber) FROM (SELECT orderNumber, SUM(cost) as costs FROM
-(SELECT orderNumber, quantityOrdered*priceEach as cost FROM orderdetails) as f
-GROUP BY orderNumber) as f1
-GROUP BY f1.costs;*/
+# 2.5
+SELECT f1.costs, COUNT(orderNumber) FROM (SELECT orderNumber, SUM(cost) as costs FROM
+    (SELECT orderNumber, quantityOrdered*priceEach as cost FROM orderdetails) as f
+    GROUP BY orderNumber) as f1
+GROUP BY f1.costs;
